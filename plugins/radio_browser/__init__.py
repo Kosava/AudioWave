1befa4a51bc35595fd615607da41e52c92f06f6d
Kosava <kosava@gmail.com>
2026-01-31 22:20:26 +0100

    cleanup: remove legacy flat structure after audiowave migration

--- plugins/radio_browser/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Radio Browser Plugin Package
-"""
-
-from .radio_browser_plugin import RadioBrowserPlugin, PLUGIN_INFO, create_plugin
-
-__all__ = ['RadioBrowserPlugin', 'PLUGIN_INFO', 'create_plugin']
